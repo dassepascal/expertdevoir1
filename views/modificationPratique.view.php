6373b22f0d3fe0133d6f0e42422867ebc3f6bfba
dassepascal <pascaldasse56@gmail.com>
2021-04-21 10:48:37 +0200

    creation de la page modification pratique

--- views/modificationPratique.view.php
+++ views/modificationPratique.view.php
@@ -5,13 +5,9 @@ require_once('controllers/Pratiques.controller.php');
 require_once('models/EcoleManager.class.php');
 require_once('controllers/Eleves.controller.php');
 $sportManager = new SportManager;
-var_dump($sportManager);
 $pratiqueManager = new PratiqueManager;
-var_dump($pratiqueManager);
 $pratiqueManager->chargementPratiques();
-
 $sportManager->chargementSports();
-
 $eleveManager = new EleveManager;
 $eleveManager->chargementEleves();
 
@@ -20,43 +16,26 @@ $eleveManager->chargementEleves();
 ob_start();
 ?>
 <form method="POST" action="<?= URL ?>pratiqueSports/av ">
-<div class="form-group">
-<label for="id_eleve">Nom : </label>
-<select name="id_eleve"     id="id_eleve">
-
-<option value=""></option>
-  <?php
-$eleves = $eleveManager->getEleves();
-  for($i =0; $i < count($eleves); $i++) :?>
-
-
-<option  value="<?= $eleves[$i]->getId() ?>"><?= $eleves[$i]->getNom(); ?></option>
-
-     <?php endfor?>
-</select><br>
-
-
-
- <label for="">Quel sport voulez-vous pratiquer ?</label><br />
- <select name="id_sport"     id="id_sport">
-
-<option value=""></option>
-  <?php
-$sports = $sportManager->getSports();
-
-  for($i =0; $i < count($sports); $i++) :?>
-
-     <option  value="<?= $sports[$i]->getId_sport()?>"><?= $sports[$i]->getNomSport(); ?></option>
-
-
-     <?php endfor?>
-</select>
-</div>
-<button type="submit" class="btn btn-success">Valider</button>
+  <div class="form-group">
+<label for="nomEleve">Nom :</label>
+<input type="text" class="form" id="nomEleve" name="nomEleve" value="<?= $pratique->getNomEleve();?>"><br/>
+<label for="nomSport">Sport actuel:</label>
+<input type="text" class="form" id="nomSport" name="nomSport" value="<?= $pratique->getNomSport();?>"><br/>
+    <label for="">Quel sport voulez-vous pratiquer ?</label><br />
+    <select name="id_sport" id="id_sport">
+      <option value=""></option>
+      <?php
+      $sports = $sportManager->getSports();
+      for ($i = 0; $i < count($sports); $i++) : ?>
+        <option value="<?= $sports[$i]->getId_sport() ?>"><?= $sports[$i]->getNomSport(); ?></option>
+      <?php endfor ?>
+    </select>
+  </div>
+  <button type="submit" class="btn btn-success">Valider</button>
 </form>
 <?php
 
-$titre = "Ajouter une Pratique";
+$titre = "Modifier une Pratique";
 $content = ob_get_clean();
 require_once('template.php');
 ?>
