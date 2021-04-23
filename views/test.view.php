ca21e9c811724e5c989e0e7aab526143f916ebca
dassepascal <pascaldasse56@gmail.com>
2021-04-23 13:50:19 +0200

    modif test

--- views/test.view.php
+++ views/test.view.php
@@ -1,1 +1,12 @@
 
+ public function suppressionPratiqueBd($id_pratique){
+    $req = "delete from pratique where id_pratique =:id_pratique ";
+$stmt=$this->getBdd()->prepare($req);
+$stmt->bindValue(":id_pratique",$id_pratique,PDO::PARAM_INT);
+$resultat = $stmt->execute();
+$stmt->closeCursor();
+
+if($resultat > 0){
+  $pratique = $this->getPratiqueById($id_pratique);
+  unset($pratique);
+}
